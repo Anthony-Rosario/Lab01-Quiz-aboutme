f049d891cf2de8ce4fbabc5c53954611882d6f84
Anthony Rosario <anthonymrosario225@gmail.com>
2021-01-11 18:56:44 -0800

    styled button/created tests

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,36 @@
-// IMPORT MODULES under test here:
-// import { example } from '../example.js';
+// IMPORT MODULES 
+import { countsAsYes, percentage } from './utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('Will return true when given input that begins with correct letter', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = countsAsYes('yes', 'Y', 'y');
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('will return false when given input that begins with correct letter', (expect) => {
+    const expected = false;
+
+    const actual = countsAsYes('no', 'n', 'N');
+
+    expect.equal(actual, expected)
+});
+
+
+test('function will return a whole number percentage of the fraction from two numbers', (expect) => {
+    const expected = 75;
+
+    const actual = percentage(3, 4);
+
     expect.equal(actual, expected);
 });
